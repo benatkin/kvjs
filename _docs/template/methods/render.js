58006e5471d6dafcc46e69d79625eddae57c6230
Ben Atkin <ben@benatkin.com>
2011-02-09 03:22:19 -0700

    brought back exploded json demo. no double rendering!

--- _docs/template/methods/render.js
+++ _docs/template/methods/render.js
@@ -5,18 +5,40 @@ function(doc) {
   
   if (typeof _ !== "function") _ = _.underscore;
   
-    _.each(this.tdoc.vars, function(value, key) {
-      ctx[key] = value.default || value.placeholder;
-    }, this);
-    _.each(this.tdoc.vars, function(value, key) {
-      if (value.default_to) {
-        ctx[key] = ctx[value.default_to];
+  _.each(this.tdoc.vars, function(value, key) {
+    ctx[key] = value.default || value.placeholder;
+  }, this);
+  _.each(this.tdoc.vars, function(value, key) {
+    if (value.default_to) {
+      ctx[key] = ctx[value.default_to];
+    }
+  }, this);
+  
+  var disp_doc = _.clone(doc);
+  delete disp_doc['_revisions'];
+  
+  var extract_multiline = function(path, obj, multiline) {
+    _.each(obj, function(val, key) {
+      if (_.isString(val)) {
+        if (val.indexOf("\n") !== -1) {
+          multiline.push({"path": path.concat(key).join("."), "str": val});
+          obj[key] = "$ref: " + path.concat(key).join(".");
+        }
+      } else if (! _.isEmpty(val)) {
+        extract_multiline(path.concat(key), val, multiline);
       }
     }, this);
-    
-    _.extend(ctx, {
-      "title": doc.env.page.title
-    });
-  ctx.inline_styles = mustache.to_html(this.tdoc.styles + "", ctx);
-  return mustache.to_html(this.tdoc.page + "", ctx);
+  }
+  
+  var path = [], multiline = [];
+  extract_multiline(path, disp_doc, multiline);
+  var json = JSON.stringify(disp_doc, null, 2);
+  _.extend(ctx, {
+    "title": doc.env.page.title,
+    "json": json,
+    "multiline": multiline
+  });
+
+  ctx.inline_styles = mustache.to_html(this.tdoc.styles, ctx);
+  return mustache.to_html(this.tdoc.page, ctx);
 }
