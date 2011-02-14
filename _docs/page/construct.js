57f9a2f6040e24860b2db10f00587e9da604a659
Ben Atkin <ben@benatkin.com>
2011-02-13 17:32:57 -0700

    indicate that template is also a type; start working on page type.

---
+++ _docs/page/construct.js
@@ -0,0 +1,9 @@
+function(doc) {
+  if (doc.env && doc.env.page && doc.env.page.template)
+    tdoc = doc.app.edocs[doc.env.page.template];
+  if (typeof tdoc !== "object" && doc.app.env && doc.app.env.app && doc.app.env.app.template)
+    tdoc = doc.app.edocs[doc.app.env.app.template];
+  if (typeof tdoc !== "object")
+    tdoc = doc.app.edocs.centerbox;
+  return new(doc.app.edoc_modules.template.Template)(tdoc, doc.app).render(doc);
+}
